294c543b7b607bac1a1e6d71873b83f5ac4df01a
Kotov584 <85634618+Kotov584@users.noreply.github.com>
2022-10-02 15:52:16 -0500

    update

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'dashboard/index.html') 
